ca9a8e2ccfbef402404aea2266e4fac764fb73c8
RailonA <cagedtreesrechnology@gmail.com>
2020-08-25 10:29:02 -0400

    First 4 functions

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+const add = (numOne, numTwo) => {
+    return numOne + numTwo;
+};
+
+const subtract = (numOne, numTwo) => {
+    return numOne - numTwo;
+};
+
+const multiply = (numOne, numTwo) => {
+    return numOne * numTwo;
+};
+
+const divide = (numOne, numTwo) => {
+    return numOne / numTwo;
+};
