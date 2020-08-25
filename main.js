f677ef94ebc24e0f312ff7ece730c1d12c02a4ee
= <=>
2020-08-25 14:11:21 -0400

    Use Case five update 2

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const CALC = document.querySelector("#calc");
 const CALC_DISPLAY = document.querySelector("#calcDisplay");
 const CALC_MEMORY = [];
+const CLEAR_BTN = document.querySelector("#clear-btn");
 
 const operations = {
     '+': (numOne, numTwo) => { return numOne + numTwo },
@@ -12,8 +13,7 @@ const operations = {
 
 const operate = (operator, numOne, numTwo) => {
     let result = operations[operator](numOne, numTwo);
-     CALC_MEMORY.splice(0,3,result);    
-     CALC_DISPLAY.innerText = result;
+     CALC_MEMORY.splice(0,3,result);   
 };
 
 
@@ -21,12 +21,15 @@ const operate = (operator, numOne, numTwo) => {
 CALC.querySelectorAll(".btn-primary").forEach( (btn) => {
     btn.addEventListener("click", (event) => {
         CALC_DISPLAY.innerText += event.target.innerText;
+        if (document.querySelector(".btn-active")) {
+            document.querySelector(".btn-active").setAttribute('class', 'btn btn-utility');
+        }
     });
 });
 
-
 CALC.querySelectorAll(".btn-operator").forEach( (btn)=>{
     btn.addEventListener("click",(event) =>{
+        event.target.setAttribute('class', 'btn btn-utility btn-active');
         CALC_MEMORY.push(parseInt(CALC_DISPLAY.innerText));
         CALC_MEMORY.push(event.target.innerText);
         CALC_DISPLAY.innerText = "";
@@ -37,7 +40,7 @@ CALC.querySelectorAll(".btn-operator").forEach( (btn)=>{
         console.log(CALC_MEMORY);
 
     }); 
-      
+    
 
 });
 
@@ -48,6 +51,7 @@ CALC.querySelectorAll(".btn-equal").forEach( (btn)=>{
         if(CALC_MEMORY.length == 3){
             operate(CALC_MEMORY[1],CALC_MEMORY[0],CALC_MEMORY[2]);
         };
+        CALC_DISPLAY.innerText = CALC_MEMORY[0];
 
         console.log(CALC_MEMORY);
     });
