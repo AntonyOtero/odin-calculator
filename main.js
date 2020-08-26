0fb8f51a95b492ac1ea824f1a35fb72186f640e3
= <=>
2020-08-26 10:11:32 -0400

    Implement floats

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const CALC_DISPLAY = document.querySelector("#calcDisplay");
 const CALC_MEMORY = [];
 const CLEAR_BTN = document.querySelector("#clear-btn");
 const EQUAL_BTN = CALC.querySelector(".btn-equal");
+const DECIMAL_BTN = CALC.querySelector(".btn-decimal");
 
 const operations = {
   "+": (numOne, numTwo) => {
@@ -21,10 +22,14 @@ const operations = {
 
 const operate = (operator, numOne, numTwo) => {
   let result = operations[operator](numOne, numTwo);
+  let decRegex = /\d*.\d+/;
+  if (decRegex.test(result)) {
+    result = result.toFixed(4);
+  }
   CALC_MEMORY.splice(0, 3, result);
 };
 
-CALC.querySelectorAll(".btn-primary").forEach((btn) => {
+CALC.querySelectorAll(".btn-number").forEach((btn) => {
   btn.addEventListener("click", (event) => {
     CALC_DISPLAY.innerText += event.target.innerText;
     if (document.querySelector(".btn-active")) {
@@ -36,8 +41,16 @@ CALC.querySelectorAll(".btn-primary").forEach((btn) => {
   });
 });
 
+DECIMAL_BTN.addEventListener("click", (event) => {
+  CALC_DISPLAY.innerText += event.target.innerText;
+  if (CALC_DISPLAY.innerText.split("").indexOf(".") > -1) {
+    DECIMAL_BTN.disabled = true;
+  }
+});
+
 CALC.querySelectorAll(".btn-operator").forEach((btn) => {
   btn.addEventListener("click", (event) => {
+    DECIMAL_BTN.disabled = false;
     event.target.setAttribute("class", "btn btn-utility btn-active");
     if (CALC_DISPLAY.innerText) {
       if (CALC_MEMORY.length == 1 && CALC_MEMORY[0] == CALC_DISPLAY.innerText) {
@@ -56,6 +69,7 @@ CALC.querySelectorAll(".btn-operator").forEach((btn) => {
 });
 
 EQUAL_BTN.addEventListener("click", (event) => {
+  DECIMAL_BTN.disabled = false;
   if (CALC_MEMORY.length == 2 && CALC_DISPLAY.innerText) {
     CALC_MEMORY.push(Number(CALC_DISPLAY.innerText));
     operate(CALC_MEMORY[1], CALC_MEMORY[0], CALC_MEMORY[2]);
